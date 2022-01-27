d46ca707a1058a383b40f8a35fb75ecbfb2d1e42
Kunal-Preet <95546419+Kunal-Preet@users.noreply.github.com>
2022-01-28 02:56:16 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,31 +1,16 @@
-const elements = {
-    h1: document.querySelector('h1'),
-    titleInput: document.querySelector('#title'),
-    textInput: document.querySelector('#text'),
-    urlInput: document.querySelector('#url'),
-    shareBtn: document.querySelector('#share'),
-  }
+window.addEventListener('DOMContentLoaded', () => {
+    const parsedUrl = new URL(window.location);
+    // searchParams.get() will properly handle decoding the values.
+    console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
+    console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
+    console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
+    const titleShared = parsedUrl.searchParams.get('title');
+    const textShared = parsedUrl.searchParams.get('text');
+    const urlShared = parsedUrl.searchParams.get('url');
+    document.querySelector('#title').innerHTML = `Title : ${titleShared}<br></br>`;
+    document.querySelector('#text').innerHTML = `Text : ${textShared}<br></br>`;
+    document.querySelector('#url').innerHTML = `URL : ${urlShared}<br></br>`;
+    
 
-  const onShare = async () => {
-    const title = elements.titleInput.value;
-    const text = elements.textInput.value;
-    const url = elements.urlInput.value;
-  
-    try {
-      if (navigator.share) {
-        await navigator.share({
-          title, text, url
-        })
-        console.log('Opened share sheet successfully', {title, text, url});
-      } else {
-        console.log('Opened share sheet successfully');
-  
-      }
-    } catch (e) {
-      console.error(`Could not open share dialog`, e);
-    }
-  }
-
-  elements.shareBtn.addEventListener('click', onShare);
-
-
+  });
+ 
